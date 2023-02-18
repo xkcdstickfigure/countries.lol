22369d5b4a96074371f37e5821aa29d17f919fa4
Archie Baer <archie@abaer.dev>
2023-02-18 23:32:14 +0000

    names

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react"
 import { GameContext } from "./context.js"
 import { Map } from "./Map.jsx"
+import names from "./names.json"
 
-export const App = () => (
-    <GameContext.Provider value={{
-        completedCountries: ["US", "RU", "CN", "CD", "FR"]
-    }}>
-        <Map />
-    </GameContext.Provider>
-)
+export const App = () => {
+    const [completedCountries, setCompletedCountries] = useState([])
+
+    return (
+        <GameContext.Provider value={{ completedCountries }}>
+            <Map />
+
+            <input onInput={e => {
+                const value = e.target.value.toLowerCase().trim()
+                const code = names[value]
+                if (code && !completedCountries.includes(code)) {
+                    setCompletedCountries([...completedCountries, code])
+                    e.target.value = ""
+                    return false
+                }
+            }} />
+
+            <p className="text-white">{completedCountries.length} / 197</p>
+        </GameContext.Provider>
+    )
+}
